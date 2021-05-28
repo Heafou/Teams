aee71a71ca8c796e33f0da5f727500147604a7bc
Heafou <62443201+Heafou@users.noreply.github.com>
2021-05-28 12:28:27 +0200

    Add files via upload

---
+++ getTeamsOwner.ps1
@@ -0,0 +1,24 @@
+﻿$credentials=Get-Credential  
+import-module microsoftTeams
+Connect-MicrosoftTeams -Credential $credentials  
+   
+$outFile="C:\Users\$env:username\Desktop\teamOwner.csv"
+$newCSV= {} | select "Equipe", "userID", "userMail", "userName" | Export-Csv $outFile
+$Teams=Get-Team  
+
+
+foreach($team in $Teams)  
+{      
+    $allOwner= Get-TeamUser -GroupId $team.GroupId -Role Owner  
+ 
+    
+    foreach($owner in $allOwner)  
+    {  
+        $userID= $owner.UserId
+        $userMail=$owner.User
+        $userName=$owner.Name
+        $teamName=$team.DisplayName 
+        add-content $outFile "$teamName,$userID,$userMail,$userName"
+    }      
+    
+}  
